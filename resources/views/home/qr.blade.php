3488d3836b82b54afd5fe8194d4ac47798cb5f60
rizanw <rzkandre@gmail.com>
2020-08-01 15:19:37 +0700

    qr scanner done

--- resources/views/home/qr.blade.php
+++ resources/views/home/qr.blade.php
@@ -20,3 +20,13 @@
         </button>
     </div>
 </div>
+<div class="col-sm-6 col-md-4">
+    <div class="card text-white bg-warning text-center">
+        <a href="{{route('qr.scan')}}" class="btn btn-warning card-body">
+            <strong class="card-bodyquote">
+                <i class="cil-camera"></i>
+                <span> Scan QR</span>
+            </strong>
+        </a>
+    </div>
+</div>
